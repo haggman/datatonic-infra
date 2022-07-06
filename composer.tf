cccf0406d4c69c1268dd030abe73143925f65afc
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-06 13:38:38 +0100

    Fix composer SA

--- composer.tf
+++ composer.tf
@@ -1,6 +1,6 @@
 // Setup the permissions for the Composer SA
 // auto created when the API is enabled
-resource "google_project_iam_member" "composer_sa" {
+resource "google_project_iam_member" "composer_sa_role" {
   project  = var.project_id
   role     = "roles/composer.ServiceAgentV2Ext"
   member   = format("serviceAccount:service-%s@cloudcomposer-accounts.iam.gserviceaccount.com", var.project_number)
