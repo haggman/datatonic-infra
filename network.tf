a25c76334a3914e7c31b632a763e767fb83eb854
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-06 12:04:57 +0100

    Add network

--- network.tf
+++ network.tf
@@ -3,3 +3,19 @@
     network in place. I manually removed it, so I could
     build the custom one.
 */
+
+//build the network, the Google blueprint makes it easy
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+
+    project_id   = var.project_id
+    network_name = "core-vpc"
+
+    subnets = [
+        {
+            subnet_name           = "sub-london"
+            subnet_ip             = "10.0.0.0/20"
+            subnet_region         = var.gcp_region
+        }
+    ]
+}
