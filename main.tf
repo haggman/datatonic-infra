12077ad3f2f9d15f6ee1b7e43bf16ed34cbf7c04
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-06 11:02:10 +0100

    Enable initial APIs

--- main.tf
+++ main.tf
@@ -1,34 +1,28 @@
-provider "google" {
+//Enable project services/APIs
+locals {
+  apis_to_enable = [    
+      "bigquery.googleapis.com",
+      "bigquerymigration.googleapis.com",
+      "bigquerystorage.googleapis.com",
+      "cloudapis.googleapis.com",
+      "cloudresourcemanager.googleapis.com",
+      "cloudtrace.googleapis.com",
+      "iam.googleapis.com",
+      "iamcredentials.googleapis.com",
+      "logging.googleapis.com",
+      "monitoring.googleapis.com",
+      "servicemanagement.googleapis.com",
+      "serviceusage.googleapis.com",
+      "storage-api.googleapis.com",
+      "storage-component.googleapis.com",
+      "storage.googleapis.com",
+      "sts.googleapis.com"
+    ]
+}
+resource "google_project_service" "enable_apis" {
+  for_each = local.apis_to_enable
   project = var.project_id
-  region  = var.gcp_region
-}
+  service = each.value
 
-/*
-    This section would be commented out initally. After building 
-    the initial infra, drop in the bucket name and redo
-    terraform init
-    to copy the state file into the bucket.
-*/
-terraform {
-  backend "gcs" {
-    bucket = "bkt-tfstate-a9fe4b60314bf1c2"
-    prefix = "terraform/state"
-  }
-}
-
-
-//build the tf_state bucket
-resource "random_id" "instance_id" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "tf_state_bucket" {
-  name          = "bkt-tfstate-${random_id.instance_id.hex}"
-  force_destroy = false
-  location      = var.gcp_region
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-
+  disable_dependent_services = true
 }
