39fb58fe420df45be5305a43e8c3eb97b33425ee
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-06 11:22:23 +0100

    Remove format check, fixed type

---
+++ provider.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = var.project_id
+  region  = var.gcp_region
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "bkt-tfstate-a9fe4b60314bf1c2"
+    prefix = "terraform/state"
+  }
+}
+
+
+//build the tf_state bucket
+resource "random_id" "instance_id" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "tf_state_bucket" {
+  name          = "bkt-tfstate-${random_id.instance_id.hex}"
+  force_destroy = false
+  location      = var.gcp_region
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+
+}
