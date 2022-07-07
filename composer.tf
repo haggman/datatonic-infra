2d3948b34d385cd704f808c0c64a81e898040cc0
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-07 15:36:38 +0100

    Switch Composer to use private IP

--- composer.tf
+++ composer.tf
@@ -8,7 +8,13 @@ resource "google_composer_environment" "pipeline_composer_instance" {
       service_account = google_service_account.composer_sa.email
       network         = module.vpc.network_id
       subnetwork      = module.vpc.subnets_ids[0]
+      //Make cluster VPC native (to support private IPs)
+      ip_allocation_policy {
+        use_ip_aliases = true
+      }
     }
+
+    //Set cluster to use private IPs
     private_environment_config {
       enable_private_endpoint = true
     }
