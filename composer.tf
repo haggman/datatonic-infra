3564d9ff3303b603014121c1f432943bebf6bc7c
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-07 20:15:11 +0100

    Create private Composer instance

--- composer.tf
+++ composer.tf
@@ -1,25 +1,25 @@
 //Create the Composer instance
-# resource "google_composer_environment" "pipeline_composer_instance" {
-#   name   = "pipeline-composer"
-#   region = var.gcp_region
+resource "google_composer_environment" "pipeline_composer_instance" {
+  name   = "pipeline-composer"
+  region = var.gcp_region
 
-#   config {
-#     node_config {
-#       service_account = google_service_account.composer_sa.email
-#       network         = module.vpc.network_id
-#       subnetwork      = module.vpc.subnets_ids[0]
-#       //Make cluster VPC native (to support private IPs)
-#       ip_allocation_policy {
-#         use_ip_aliases = true
-#         cluster_secondary_range_name = "composer-cluster-pods"
-#         services_secondary_range_name = "composer-cluster-services"
-#       }
-#     }
+  config {
+    node_config {
+      service_account = google_service_account.composer_sa.email
+      network         = module.vpc.network_id
+      subnetwork      = module.vpc.subnets_ids[0]
+      //Make cluster VPC native (to support private IPs)
+      ip_allocation_policy {
+        use_ip_aliases = true
+        cluster_secondary_range_name = "composer-cluster-pods"
+        services_secondary_range_name = "composer-cluster-services"
+      }
+    }
 
-#     //Set cluster to use private IPs
-#     private_environment_config {
-#       enable_private_endpoint = true
-#       master_ipv4_cidr_block = "10.40.0.0/28"
-#     }
-#   }
-# }
+    //Set cluster to use private IPs
+    private_environment_config {
+      enable_private_endpoint = true
+      master_ipv4_cidr_block = var.composer_master_cidr
+    }
+  }
+}
