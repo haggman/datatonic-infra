3fea07dc021b8f0b5bbb2659b73f7bba1421fd8f
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-07 11:56:47 +0100

    Add access for students

--- iam.tf
+++ iam.tf
@@ -1,4 +1,5 @@
 locals {
+    //my list of users and their corresponding roles
   user_roles = {
     "nan.kang@datatonic.com" = ["roles/browser"],
     "abdikarim.dilib@datatonic.com" = ["roles/browser"],
@@ -12,15 +13,37 @@ locals {
     "sabah.hussain@datatonic.com" = ["roles/browser"],
   }
 
+  /*
+    Builds an structure that looks like the following, with an entry
+    for each user, and each role they have
+    [
+        {
+            "role" = "roles/browser"
+            "user" = "abdikarim.dilib@datatonic.com"
+        },
+        {
+            "role" = "roles/browser"
+            "user" = "alice.staton@datatonic.com"
+        },...
+  */
   privliges = flatten([
-    for user_key, role in local.user_roles : [
-        {user_email = user_key,role = role}
+    for user_key, roles in local.user_roles : [
+        for role in roles :
+        {
+            user = user_key,
+            role = role
+        }
     ]
   ])
-  
 }
 
-output "users" {
-  value = local.privliges
-}
+# output "roles" {
+#   value = { for entry in local.privliges: "${entry.user}.${entry.role}" => entry }
+# }
 
+resource "google_project_iam_member" "user_roles" {
+  for_each = { for entry in local.privliges: "${entry.user}.${entry.role}" => entry }
+  project = var.project_id
+  role    = each.value.role
+  member  = format("user:%s", each.value.user)
+}
