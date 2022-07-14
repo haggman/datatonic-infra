1188ec5024eb74f8cb35067f2ec6ad25d71828b1
Patrick Haggerty <hagg@haggconsulting.com>
2022-07-14 10:36:52 -0500

    Add staging GCS bucket

---
+++ storage.tf
@@ -0,0 +1,15 @@
+//Build a staging bucket
+
+
+module "gcs_buckets" {
+  source  = "terraform-google-modules/cloud-storage/google"
+  project_id  = var.project_id
+  names = ["data_staging"]
+  prefix = "bkt"
+  location = var.gcp_region
+  randomize_suffix = true
+
+  set_admin_roles = true
+  admins = [google_service_account.run_sa.email]
+
+}
